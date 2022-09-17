a5c022c0cdf3e00ad91b8f466d77c0d6d856a4e5
robert-vaccaro <robertvaccarotech@gmail.com>
2022-09-17 14:36:41 -0700

    made separate tab html views

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,51 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// // Get the modal
+// var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var btn2 = document.getElementById("myBtn2");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// var homeNav = document.getElementsByClassName("home-nav");
+// var servicesNav = document.getElementsByClassName("services-nav");
+// var trainingsNav = document.getElementsByClassName("trainings-nav");
+// var contactNav = document.getElementsByClassName("contact-nav");
+// var aboutNav = document.getElementsByClassName("about-nav");
+// var galleryNav = document.getElementsByClassName("gallery-nav");
+// // Get the button that opens the modal
+// var contactHome = document.getElementById("contactHome");
+// var contactHome2 = document.getElementById("contactHome2");
+// var contactAbout = document.getElementById("contactAbout");
+// var contactGallery = document.getElementById("contactGallery");
+// var contactServices = document.getElementById("contactServices");
+// var contactTrainings = document.getElementById("contactTrainings");
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-btn2.onclick = function() {
-    modal.style.display = "block";
-  }
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// homeNav.onclick = function() {
+//   clearNavTextColor()
+//   homeNav[0].classList.add("active");
+//   console.log()
+// }
+// servicesNav.onclick = function() {
+//   clearNavTextColor()
+//   servicesNav[0].classList.add("active");
+// }
+// trainingsNav.onclick = function() {
+//   clearNavTextColor()
+//   trainingsNav[0].classList.add("active");
+// }
+// contactNav.onclick = function() {
+//   clearNavTextColor()
+//   contactNav[0].classList.add("active");
+// }
+// aboutNav.onclick = function() {
+//   clearNavTextColor()
+//   aboutNav[0].classList.add("active");
+// }
+// galleryNav.onclick = function() {
+//   clearNavTextColor()
+//   galleryNav[0].classList.add("active");
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// var clearNavTextColor = () => {
+//   homeNav[0].classList.remove("active");
+//   servicesNav[0].classList.remove("active");
+//   trainingsNav[0].classList.remove("active");
+//   contactNav[0].classList.remove("active");
+//   aboutNav[0].classList.remove("active");
+//   galleryNav[0].classList.remove("active");
+// }
